f6663da883743696e427b281f866f24a2b1715e7
César González <48808936+CesarAntonioGonzalez@users.noreply.github.com>
2023-08-09 10:08:02 -0400

    Conexión a DB
    Implementación de conector a DB Sql

---
+++ src/api/models/sever.js
@@ -0,0 +1,12 @@
+module.exports = {
+    user: 'cesar',
+    password: 'ce123456',
+    server: 'localhost\\SQLEXPRESS',
+    database: 'ApiHRConnect',
+    options:{
+        trustedconnection: true,
+        enableArithAbort: true,
+        instancename : 'SQLEXPRESS',
+        trustServerCertificate: true,
+    },
+  };

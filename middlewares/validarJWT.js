3dbe5fedfdb157b3aac21426d512a34328f2eadc
César González <48808936+CesarAntonioGonzalez@users.noreply.github.com>
2023-08-11 15:04:44 -0400

    Generador y Validardor de token

---
+++ middlewares/validarJWT.js
@@ -0,0 +1,29 @@
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+
+const validarJWT = async (req = request, res = response, next) => {
+
+    const token = req.header('x-token');
+
+    if(!token){
+        return res.status(401).json({
+            msg: 'No hay token en la peticion'
+        })
+    }
+
+    try {
+        jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+    }
+
+}
+
+module.exports = {
+    validarJWT
+}
